8c264d5b39a8e9c19009dda2173b85ee20cfdb4e
LeoDomGit <100814585+LeoDomGit@users.noreply.github.com>
2022-12-03 09:16:29 +0700

    Commit

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -20,11 +20,12 @@
     <div class="l-navbar" id="nav-bar">
         <nav class="nav">
             <div> <a href="#" class="nav_logo"> <i class='bx bx-layer nav_logo-icon'></i> <span
-                        class="nav_logo-name">BBBootstrap</span> </a>
+                        class="nav_logo-name">Dashboard</span> </a>
                 <div class="nav_list"> <a href="#" class="nav_link active"> <i
                             class='bx bx-grid-alt nav_icon'></i> <span class="nav_name">Dashboard</span> </a> <a
                         href="#" class="nav_link"> <i class='bx bx-user nav_icon'></i> <span
-                            class="nav_name">Users</span> </a> <a href="#" class="nav_link"> <i
+                            class="nav_name">Users</span> </a> 
+                            <a href="#" class="nav_link"> <i
                             class='bx bx-message-square-detail nav_icon'></i> <span class="nav_name">Messages</span>
                     </a> <a href="#" class="nav_link"> <i class='bx bx-bookmark nav_icon'></i> <span
                             class="nav_name">Bookmark</span> </a> <a href="#" class="nav_link"> <i
--- resources/views/roles/index.blade.php
+++ resources/views/roles/index.blade.php
@@ -1,5 +1,7 @@
 @extends('layout.layout')
 @section('header','Quản lý loại tài khoản')
 @section('main')
-
+    <div class="mt-2">
+        <button class="btn btn-primary">Thêm loại tài khoản</button>
+    </div>
 @endsection
